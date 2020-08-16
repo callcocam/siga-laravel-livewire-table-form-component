8d9a9c13970ef0193a3b2ce041b50a2200b02b8d
callcocam <callcocam@gmail.com>
2020-08-16 00:54:26 -0300

    nova views

--- src/LaravelLivewireTables/TablesServiceProvider.php
+++ src/LaravelLivewireTables/TablesServiceProvider.php
@@ -31,7 +31,7 @@ class TablesServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__.config('lw-tables-config.load-views','/../../resources/views/livewire/table'), 'lw-tables-views');
         $this->loadRoutesFrom(__DIR__ . '/../../routes/table.php');
 
-        $this->publishes([__DIR__ . '/../../config/lw-tables-config.php' => config_path('lw-tables.php')], 'lw-tables-config');
+        $this->publishes([__DIR__ . '/../../config/lw-tables-config.php' => config_path('lw-tables-config.php')], 'lw-tables-config');
         $this->publishes([__DIR__ . '/../../resources/views/livewire/table' => resource_path('views/livewire/vendor/lw-tables')], 'lw-tables-views');
     }
 }
