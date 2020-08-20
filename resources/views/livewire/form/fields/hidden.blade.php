4a75b990d0c60f7c5796ee03904b602d38387a7f
callcocam <callcocam@gmail.com>
2020-08-20 00:31:23 -0300

    retorna null

--- resources/views/livewire/form/fields/hidden.blade.php
+++ resources/views/livewire/form/fields/hidden.blade.php
@@ -2,7 +2,7 @@
    <div class="col-md">
         <input
             id="{{ $field->name }}"
-            type="{{ $field->input_type ?? 'hidden' }}"
+            type="{{ $field->input_type }}"
             wire:model="{{ $field->key }}">
     </div>
 </div>
